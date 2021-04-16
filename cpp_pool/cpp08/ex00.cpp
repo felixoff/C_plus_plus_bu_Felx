4e778b603bd7d8077c8a4004493f79e148d26573
Nikita <N.Felixoff@yandex.ru>
2021-04-16 13:27:35 +0300

    days0-7

---
+++ cpp_pool/cpp08/ex00.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include <list>
+#include <string>
+#include<iterator>
+template <class T>
+class T::iterator easyfind(T &container, int elem);
+
+template <class T>
+class T::iterator easyfind(T &container, int elem)
+{
+    class T::iterator start = container.begin();
+    class T::iterator end = container.end();
+    while(start != end)
+    {
+        if (*start == elem)
+            return start;
+    }
+    return end;
+}
+
+int				main(void)
+{
+	std::list<int>::iterator	result;
+	std::list<int>				lst;
+
+	lst.push_back(1);
+	lst.push_back(7);
+	lst.push_back(21);
+	lst.push_back(84);
+
+	std::cout << "Search the value 21: ";
+	result = easyfind(lst, 21);
+	if (result != lst.end())
+		std::cout << *result << std::endl;
+	else
+		std::cout << "Not found" << std::endl;
+
+	std::cout << "Change the found value to 42" << std::endl;
+	*result = 42;
+	std::cout << "Search the value 21: ";
+	result = easyfind(lst, 21);
+	if (result != lst.end())
+		std::cout << *result << std::endl;
+	else
+		std::cout << "Not found" << std::endl;
+
+	std::cout << "Search the value 42: ";
+	result = easyfind(lst, 42);
+	if (result != lst.end())
+		std::cout << *result << std::endl;
+	else
+		std::cout << "Not found" << std::endl;
+	return (0);
+}
